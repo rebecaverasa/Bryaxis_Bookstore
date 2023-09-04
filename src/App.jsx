dc28a2466f9d8894d0f03885100cb55a8848c860
rebecaverasa <rebecaverasa@gmail.com>
2023-09-04 14:55:20 -0300

    Firebase configured.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ItemListContainer from "./components/ItemListContainer";
 import Navbar from "./components/NavBar";
 import CartPage from "./components/CartPage";
-import CartProvider from "./components/CartContext"; // Importe o CartProvider
+import CartProvider from "./components/CartContext";
+import ItemSearch from './components/ItemSearch';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<ItemListContainer />} />
           <Route path="/Bryaxis" element={<ItemListContainer />} />
           <Route path="/cart" element={<CartPage />} />
+          <Route path="/item/:id" element={<ItemSearch />} />
         </Routes>
       </CartProvider>
     </Router>
